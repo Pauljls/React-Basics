788120a68e57e6c753cc5605790b83b0f73fd425
Pauljls <paulazarsol@gmail.com>
2024-05-26 21:16:13 -0500

    Learning css modules to use in components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,8 @@
 import { ReactNode, useState } from "react"
-import './Button.css'
+//para crear un modulo de css sera necesario importarlo bajo una costante
+//los modulos seran las propeidades del objeto por lo que sera necesario nombrarlas
+//de la siguiente forma ".nombre"
+import style from './Button.module.css'
 
 type Props = {
     children : ReactNode
@@ -8,14 +11,19 @@ type Props = {
 }
 
 
+console.log(style)
 function Button({children, onClick, isLoading} : Props){
 
 
+    const className = [`btn btn-${isLoading ? 'secondary' : 'primary'}`,style.button, style.padded].join(" ")
+    //Los modulso de css iran en el apartado de clases
     return (<button 
         disabled={isLoading} 
         onClick={onClick} 
         type="button" 
-        className= {`btn btn-${isLoading ? 'secondary' : 'primary'}`}>
+        className= {className}
+        //className={[style.button, style.padded].join(" ")}
+        >
             {isLoading ? 'Cargando': children }
         </button>)
 }
